9b3b1ff2acac3b4507b6a5f3e83ddf2420911c82
dorusrutten <dorusrutten@icloud.com>
2023-02-02 15:16:03 +0100

    ja

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include "pins_arduino.h"
 
+#define GC 8
+Servo boob;
+int ass = 1;
+int knop = 3;
+int button;
+byte state;
 void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Code has started!");
+
   
+  pinMode(GC, OUTPUT);
+
+  //boob.attach(2);
+  pinMode(8,INPUT);
 }
 
 void loop() {
   
+  state = digitalRead(GC);
+   Serial.print(state);
+  
+  if(digitalRead(8) == HIGH){
+  //boob.write(0);
+     boob.attach(2);
+  delay(1000);
+   Serial.print("1oob");
+ }
+else {
+  delay(1000);
+   Serial.print("dor");
+   boob.attach(3);
+
+}
+
 }
