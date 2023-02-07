d8ea4dde0550a2199328b4e1b86bbec8f5b33a28
dorusrutten <dorusrutten@icloud.com>
2023-02-07 10:17:45 +0100

    Co-authored-by: Deedikjepijn <Deedikjupijn@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <Servo.h>
 #include "pins_arduino.h"
 
-#define GC 8 /*pin 8 is defined as GC, geen idee waarvoor omdat we de pins to uitlezen bij de if/else functie */
-Servo boob; /*sets the servo (library) to boob*/
+Servo boob1;
+Servo boob2;
+Servo boob3;
+Servo boob4;
 int ass = 1;
 int knop = 3;
 int button;
@@ -13,29 +15,49 @@ void setup() {
   Serial.begin(9600);
   Serial.println(); /*als je het mij vraagt doet dit ook niks en kan je dit verwijderen!!!!!!!!!!!!!!!!!!!!!!!*/
   Serial.println("Code has started!"); /*Prints in the serial monitor page, this is to make sure that the code actually ran xd*/
-  pinMode(GC, OUTPUT); /*Refer to Line 5, no idea what this does right now*/
-  pinMode(8,INPUT); /*Set pin 8 as input*/
+  pinMode(2,OUTPUT); /*Set pin 2 as an output*/
+  pinMode(3,OUTPUT); /*Set pin 3 as an output*/
+  pinMode(4,OUTPUT);  /*Set pin 4 as output*/
+  pinMode(5,OUTPUT); /*Set pin 5 as output*/
+  pinMode(8,INPUT); /*Set pin 8 as an input*/
 }
 
 // loop with checking if the power is going back to the arduino and changing the state of the servomotor
 void loop() {  
-  state = digitalRead(GC);
-  Serial.print(state);
 
 // if the digital read from pin 8 is high (prob. anything higher then 0) then...
 if(digitalRead(8) == HIGH){
-  //boob.write(0); no fucking clue what did does/did or is suppost to do!!!!!!!!!!!!!!!!!
-  boob.attach(2); /*Enable pin2 for the servo connection*/
-  delay(1000);
-  Serial.print("Hell yeaahhh I'm turned on");
+  boob1.attach(2); /*Enable pin 2 for the servo connection*/  
+  boob2.attach(3); /*Enable pin 3 for the servo connection*/
+  boob3.attach(4);// ebenbles pin 4 fir teh survo connection
+  boob4.attach(5); /*Enable pin 5 for the servo connection*/
+  delay(1);
+  Serial.print("on!");
 }
+
+if(digitalRead(9) == HIGH){
+Serial.print("knop 2 aan");
+boob1.attach(2);
+boob2.attach(3);
+boob2.write(180);
+}
+
+// if(digitalRead(10) == HIGH){
+//   Serial.print("knop 3 aan");
+//   boob2.attach(3);
+//   boob2.attach(4);
+// } 
 
 // if pin 8 is anything except HIGH (prob 1) then...
 else {
-  delay(1000);
-  Serial.print("No movement means turned off, instantly");
-  boob.attach(3); /*Pin 3 is now sending data to the servo to rotate (Disconnected)*/
-}
+  delay(1);
+  Serial.print("off!");
+  boob1.detach();
+  boob2.detach();
+  boob3.detach();
+  boob4.detach();
+
+  }
 
 // end loop
 } 
